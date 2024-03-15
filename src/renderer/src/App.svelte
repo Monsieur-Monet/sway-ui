4fe4de86541b4f51beb4a28056a329faa8a0286d
Monsieur-Monet <mail@tscheer.dev>
2024-03-15 16:45:24 +0100

    init

---
+++ src/renderer/src/App.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  import Versions from './components/Versions.svelte'
+  import './app.css'
+</script>
+
+<div class="app flex flex-col">
+  <h1>Hello, Electron!</h1>
+  <p>Welcome to your Electron application.</p>
+</div>
+
+<Versions />
