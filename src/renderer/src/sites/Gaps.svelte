e176be811a9bf7c8d4a1b84b161cc67d71f9dc6b
Monsieur-Monet <mail@tscheer.dev>
2024-03-25 16:00:26 +0100

    added several new svelte components and the config file.

---
+++ src/renderer/src/sites/Gaps.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  import gapsData from '../config.json'
+  
+</script>
+
+{#if gapsData}
+  <div>
+    <p>Inner Gap Value: {gapsData.gaps.inner.command}</p>
+    <p>Outer Gap Value: {gapsData.gaps.outer.command}</p>
+    <p>{configPath}</p>
+  </div>
+{/if}
---
+++ src/renderer/src/sites/Home.svelte
@@ -0,0 +1,4 @@
+<div>
+  <h1 class="text-4xl font-bold">Welcome to the Home Page</h1>
+  <div>first please select your config file:</div>
+</div>
